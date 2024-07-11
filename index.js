c29b4ea32d973f759d2ad4837dbd55af7482d58e
satish0036 <satish0036kumar@gmail.com>
2024-07-11 20:40:56 +0530

    cors update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 
 app.use(cors({
-    origin: ['http://localhost:3000','/',"http://localhost:5173",'https://resume.indiaarticle24.com'],
+    origin: ['http://localhost:3000','/',"http://localhost:5173",'https://resume.indiaarticle24.com','https://airesumes.netlify.app'],
     credentials: true,
   }));
   
