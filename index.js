705b5c6da50696bccbb2f289abdbe8cc0e949e97
satish0036 <satish0036kumar@gmail.com>
2024-07-18 11:42:37 +0530

    update in cors

--- index.js
+++ index.js
@@ -4,24 +4,29 @@ import AuthRoutes from "./routes/AuthRoutes.js"
 import cors from "cors"
 import AddResumeRoutes from './routes/AddResumeRoutes.js';
 // import cookieParser from "cookie-parser";
-const PORT=process.env.PORT || 8800;
-const app=Express();
+const PORT = process.env.PORT || 8800;
+const app = Express();
 
 dotenv.config();
 
 
 app.use(cors({
-    origin: ['http://localhost:3000','/',"http://localhost:5173",'https://resume.indiaarticle24.com','https://airesumes.netlify.app'],
-    credentials: true,
-  }));
-  
+  origin: ['http://localhost:3000', '/'
+    , "http://localhost:5173",
+    'https://resume.indiaarticle24.com',
+    'https://airesumes.netlify.app',
+    'https://nextjs-resume-builder-pink.vercel.app'
+  ],
+  credentials: true,
+}));
+
 //   app.use(cookieParser())
-  app.use(Express.json())
+app.use(Express.json())
 
 
-app.use("/api/auth",AuthRoutes)
-app.use("/api/resume",AddResumeRoutes)
+app.use("/api/auth", AuthRoutes)
+app.use("/api/resume", AddResumeRoutes)
 
-app.listen(PORT,(req,res)=>{
-    console.log("connected to Backend")
+app.listen(PORT, (req, res) => {
+  console.log("connected to Backend")
 })
